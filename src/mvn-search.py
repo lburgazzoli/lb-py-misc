cc78ccc3765135496466221fdece8f19cc007270
lburgazzoli <lburgazzoli@gmail.com>
2014-12-11 15:26:57 +0100

    fix

--- src/mvn-search.py
+++ src/mvn-search.py
@@ -4,18 +4,15 @@ import requests
 import os
 import sys
 
-params  = { 'rows': '20', 'wt': 'json', 'q': sys.argv[1]}
-proxies = {}
+pars = {'rows': '20', 'wt': 'json', 'q': sys.argv[1]}
+prxs = {}
 
 if os.environ.get('http_proxy'):
-    proxies['http']  = os.environ.get('http_proxy')
+    prxs['http'] = os.environ.get('http_proxy')
 if os.environ.get('http_proxy'):
-    proxies['https'] = os.environ.get('https_proxy')
+    prxs['https'] = os.environ.get('https_proxy')
     
-r = requests.get("http://search.maven.org/solrsearch/select", 
-    params  = params, 
-    proxies = proxies
-)
+r = requests.get("http://search.maven.org/solrsearch/select", params=pars, proxies=prxs)
 
 for result in r.json()['response']['docs']:
     print("{}:{}:{}".format(result['g'], result['a'], result['latestVersion']))
